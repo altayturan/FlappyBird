3f94a7b3af67fb5b9066e45d9c8cd361ed02b70f
Altay Turan <altayturan@Altays-MacBook-Pro.local>
2025-02-23 00:45:40 +0300

    feat: Create pipe moving mechanism

--- Assets/_Project/Scripts/Boru.cs
+++ Assets/_Project/Scripts/Boru.cs
@@ -1,6 +1,44 @@
+using System;
 using UnityEngine;
 
 public class Boru : MonoBehaviour
 {
+    private float _speed;
+    private float _destroyXPosition;
+    private Action _onDestroy;
     
+    public void Initialize(float moveSpeed, float destroyPos, Action onDestroy)
+    {
+        _speed = moveSpeed;
+        _destroyXPosition = destroyPos;
+        _onDestroy = onDestroy;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        if (transform.position.x < _destroyXPosition)
+        {
+            _onDestroy?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+
+    public void UpdateSpeed(float newSpeed)
+    {
+        _speed = newSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Controller controller = FindObjectOfType<Controller>();
+            if (controller != null)
+            {
+                controller.StopGame();
+            }
+        }
+    }
+
 }
--- Assets/_Project/Scripts/Controller.cs
+++ Assets/_Project/Scripts/Controller.cs
@@ -3,5 +3,81 @@ using System.Collections.Generic;
 
 public class Controller : MonoBehaviour
 {
+    [Header("Pipe Settings")]
+    [SerializeField] private Boru pipePrefab;
+    [SerializeField] private float pipeSpeed = 5f;
+    [SerializeField] private float desiredPipeSpacing = 2f;
+    [SerializeField] private float minHeight = -2f;
+    [SerializeField] private float maxHeight = 2f;
+    [SerializeField] private float xSpawnPosition = 10f;
+    [SerializeField] private float destroyXPosition = -10f;
     
+    [Header("Difficulty Settings")]
+    [SerializeField] private float speedIncreaseRate = 0.1f; // Speed increase per second
+    [SerializeField] private float maxPipeSpeed = 15f; // Maximum speed limit
+    [SerializeField] private float gameTime; // Track how long the game has been running
+
+
+    private float _timer;
+    private bool _isGameActive = true;
+    private float _currentPipeSpeed;
+    private float _currentSpawnInterval;
+
+    private List<Boru> _pipes = new();
+
+
+    private void Start()
+    {
+        _currentPipeSpeed = pipeSpeed;
+        gameTime = 0f;
+        CalculateSpawnInterval();
+        _timer = _currentSpawnInterval; // Start spawning immediately
+
+    }
+
+    private void Update()
+    {
+        if (!_isGameActive) return;
+
+        gameTime += Time.deltaTime;
+        UpdateDifficulty();
+
+        _timer -= Time.deltaTime;
+        if (_timer <= 0)
+        {
+            SpawnPipe();
+            _timer = _currentSpawnInterval;
+        }
+    }
+
+    private void UpdateDifficulty()
+    {
+        _currentPipeSpeed = pipeSpeed + (int)(gameTime / 30) * speedIncreaseRate ;
+        _currentPipeSpeed = Mathf.Clamp(_currentPipeSpeed, pipeSpeed, maxPipeSpeed);
+        _pipes.ForEach(x=>x.UpdateSpeed(_currentPipeSpeed));
+        
+        CalculateSpawnInterval();
+    }
+    private void CalculateSpawnInterval()
+    {
+        _currentSpawnInterval = desiredPipeSpacing / _currentPipeSpeed;
+    }
+    private void SpawnPipe()
+    {
+        float randomHeight = Random.Range(minHeight, maxHeight);
+        Vector3 spawnPosition = new Vector3(xSpawnPosition, randomHeight, 0);
+        Boru newPipe = Instantiate(pipePrefab, spawnPosition, Quaternion.identity);
+        _pipes.Add(newPipe);
+        newPipe.Initialize(_currentPipeSpeed
+            , destroyXPosition,(() =>
+            {
+                _pipes.Remove(newPipe);
+            }));
+    }
+
+    public void StopGame()
+    {
+        _isGameActive = false;
+    }
+
 }
